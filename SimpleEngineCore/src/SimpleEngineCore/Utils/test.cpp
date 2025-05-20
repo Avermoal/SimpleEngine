5d96c0e5567ba59ce7fba05205e462f8866c482d
Avermoal <misho54@mail.ru>
2025-05-20 17:08:05 +0600

    First commit

---
+++ SimpleEngineCore/src/SimpleEngineCore/Utils/test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "SimpleEngineCore/Utils/test.hpp"
+
+namespace SimpleEngine {
+	void sayHello() {
+		std::cout << "Hello form Simple Engine Core" << std::endl;
+	}
+}
---
+++ SimpleEngineEditor/src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <SimpleEngineCore/Utils/test.hpp>
+
+
+int main(void) {
+
+	std::cout << "Hello World!" << std::endl;
+
+	SimpleEngine::sayHello();
+
+	std::cin.get();
+	
+	return 0;
+}
